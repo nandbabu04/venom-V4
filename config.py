1d1f9734fd1df0255976c99b2d8ce9a96c480b66
nandbabu04 <71744227+nandbabu04@users.noreply.github.com>
2024-07-19 15:13:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("6753970128:AAGKkXe7lVI4F-w4d2nDo5vkqzB3eWGCm9g")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("something_u_know")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("1786683163")
 
 
 
